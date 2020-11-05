fc6bd948e8abc1210048f5be11b702ef9bb85bd3
Alejandro Cesar Moya <46249970+e1ectriccrasher@users.noreply.github.com>
2020-11-04 21:04:22 -0600

    sd

--- Source/hashTable.java
+++ Source/hashTable.java
@@ -1,56 +1,89 @@
-import java.util.Hashtable;
+import java.util.ArrayList;
 
-
-
-public class hashTable <T>{
-
-    int num;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + num;
-        return result;
+public class hashTable<T> {
+	private ArrayList<T>[] tabla;
+	int cont;
+	double factorCarga=20000;
+		
+public hashTable() {
+		tabla = new ArrayList[100];
+		cont = 0;
+			
+		for(int i = 0; i < tabla.length; i++)
+			tabla[i] = new ArrayList<T>();
+    }
+    
+    public int funcionMisteriosa(T elem) {
+        return (int) ((int) elem * .05);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        hashTable other = (hashTable) obj;
-        if (num != other.num)
-            return false;
-        return true;
-    }
+	public void insert(T elem) {
+	//if(muyGrande())
+			//expande();
+		if ((cont/tabla.length) > factorCarga)
+			expande();
+	
+        int pos = funcionMisteriosa(elem);
+		tabla[pos % tabla.length].add(elem);
+		cont++;		
+	}
+		
+	public T buscar(T elem) {
+		int pos = funcionMisteriosa(elem);
+		int index = tabla[pos % tabla.length].indexOf(elem);
 
+		if(index < 0)
+			return null;
+		else 
+			return elem;
+	}
+		
+	public boolean borrar(T elem) {
+		boolean res = false;
+		int pos = funcionMisteriosa(elem);
+
+		if(tabla[pos % tabla.length].contains(elem)) {
+			tabla[pos % tabla.length].remove(elem);
+			cont--;
+			res = true;
+		}
+
+		return res;
+	}
+
+	private void expande() {
+		ArrayList<T>[] nuevaTabla = new ArrayList[tabla.length * 2];
+		
+		//Inicializar listas
+		for(int i = 0; i < nuevaTabla.length;i++)
+			nuevaTabla[i] = new ArrayList<T>();
+			
+		//Mover elementos
+		for(ArrayList<T> lista : tabla) {
+			for(T elem : lista) {
+				// Volvemos a hashear
+				int pos = funcionMisteriosa(elem);
+				nuevaTabla[pos % nuevaTabla.length].add(elem);
+			}
+		}
+
+		tabla = nuevaTabla;
+    }
+    
     public static void main(String[] args) {
-        Hashtable<Integer, Integer> h = new Hashtable<Integer, Integer>();
+        hashTable tabla = new hashTable<>();
 
         long start = System.currentTimeMillis();
-        for(int i=0;i<100000;i++){
-            h.put(h.hashCode(), i);
-            System.out.println("\n" + "add" + Integer.toString(i));
+        for(int i=0;i<100000; i++){
+            tabla.insert(i);
+            System.out.println("inserto   " +  Integer.toString(i));
         }
         long end = System.currentTimeMillis();
 
-        float sec =(end - start) / 100F;
+        float sec = (end - start) / 1000F;
 
         System.out.println(sec);
-        
-
-        
-        
-        
     }
-
-    
-
-
-
-
+		
 }
+
